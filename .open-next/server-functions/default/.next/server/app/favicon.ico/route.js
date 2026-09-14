0cd60961e94255dac9b260d3cc6beb88c002888d
Ashutosh Kumar <ashukumar3436@gmail.com>
2026-09-14 16:35:52 +0530

    cloudflare config

---
+++ .open-next/server-functions/default/.next/server/app/favicon.ico/route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/[externals]__0l8ei7u._.js")
+R.c("server/chunks/[root-of-the-server]__1sv8dgq._.js")
+R.c("server/chunks/_next-internal_server_app_favicon_ico_route_actions_0g2jjls.js")
+R.m(6882)
+module.exports=R.m(6882).exports
---
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YhqNDKIOmHwmUbRUGb64Tq7tof10cf/fycS0KITtt5o=\"\n}"
